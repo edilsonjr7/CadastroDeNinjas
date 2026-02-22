9a9a38d58bf9f7e06f7142cb080f9cb903436b63
unknown <edilsondesouzalimajunior10@gmail.com>
2026-02-22 16:33:44 -0300

    Arquivo NinjaModel e JPA adicionados

--- CadastroDeNinjas/src/main/java/dev/java/Cadastro/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java/Cadastro/NinjaController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController // controlador para APIRest
+@RestController // controlador para APIRest 
 @RequestMapping("/mensagem")  //aqui eu faço o controle de rotas na minha API
-public class Controller {
+public class NinjaController {
 
-    @GetMapping// aqui quem acessa meu "boasvindas" mostrará a mensagem
+    @GetMapping// aqui quem acessa minha porta "mensagem" mostrará a mensagem
     public String boaVindas(){
         return "Qualquer pessoa pode acessar essa mensagem ao seguir a rota /mensagem";
     }
---
+++ CadastroDeNinjas/src/main/java/dev/java/Cadastro/NinjaModel.java
@@ -0,0 +1,69 @@
+package dev.java.Cadastro;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//JPA = Java Persistence API
+@Entity // o Entity transforma toda a classe em uma entidade do meu banco de dados
+@Table(name="cadastro_ninja")// ele coloca o nome da minha tabela 
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String nome;
+    String email;
+    int idade;
+
+        public NinjaModel(){
+
+    }
+
+    public NinjaModel(String nome, String email, int idade){
+        this.nome=nome;
+        this.email=email;
+        this.idade=idade;
+
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+
+
+
+
+    
+}
